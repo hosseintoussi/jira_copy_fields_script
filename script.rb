e03e8e46b8cef7bb6fe4f8bcf02f02c6900517f5
hosseintoussi <hossein@mindvalley.com>
2015-09-18 10:17:29 +0800

    Adding a config file, to make inputs easier and cleaner

--- script.rb
+++ script.rb
@@ -1,35 +1,49 @@
 require 'net/https'
 require 'JSON'
+require 'yaml'
 
-uri = URI('https://mindvalley.atlassian.net/rest/api/2/search?jql=project="PLAY"&maxResults=10')
-saeed = ""
+config = YAML.load_file('config.yml')
+
+project_url = "https://mindvalley.atlassian.net/rest/api/2/search?jql=project='#{config['project']}'&maxResults=#{config['max_results']}"
+uri = URI(project_url.strip)
+all_issues = ""
+# puts "============magic==========="
+# puts project_url
+# puts config['project']
+# puts config['username']
+# puts config['password']
+# puts config['old_custom_field']
+# puts config['new_custom_field']
+# puts "======================="
+
+
 Net::HTTP.start(uri.host, uri.port,
                 :use_ssl => uri.scheme == 'https',
 :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
 
   request = Net::HTTP::Get.new uri.request_uri
-  request.basic_auth 'hossein', 'sa7269230'
+  request.basic_auth "#{config['username']}", "#{config['password']}"
 
   response = http.request request # Net::HTTPResponse object
 
-  saeed = JSON.parse(response.body)
+  all_issues = JSON.parse(response.body)
 end
 
-puts "Total issues: #{saeed["total"]}"
+puts "Total issues: #{all_issues["total"]}"
 
-saeed["issues"].each do |issue|
+all_issues["issues"].each do |issue|
   puts "======================="
-  if !issue["fields"]["customfield_11100"].nil?
+  if !issue["fields"]["#{config['old_custom_field']}"].nil?
+    old_field_value = issue["fields"]["#{config['old_custom_field']}"]
+    new_field = config['new_custom_field']
     url = "https://mindvalley.atlassian.net/rest/api/2/issue/#{issue["key"]}"
     uri1 = URI(url.strip)
-    puts "#{issue["key"]} has the custom due date of: #{issue["fields"]["customfield_11100"]}"
-    duedate = issue["fields"]["customfield_11100"]
-    puts "due date is set to: #{duedate}"
+    puts "#{config['old_custom_field']} of #{issue["key"]} has the value of: #{old_field_value}"
 
     req = Net::HTTP::Put.new uri1.request_uri
-    req.basic_auth 'hossein', 'sa7269230'
+    req.basic_auth "#{config['username']}", "#{config['password']}"
     req['Content-Type'] = 'application/json'
-    req.body = "{\"fields\":{\"duedate\":\"#{duedate}\"}}"
+    req.body = "{\"fields\":{\"#{new_field}\":\"#{old_field_value}\"}}"
     Net::HTTP.start(uri1.host, uri1.port,
                     :use_ssl => uri.scheme == 'https',
     :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
@@ -37,7 +51,7 @@ saeed["issues"].each do |issue|
       puts response
     end
   else
-    puts "Skipping for #{issue["key"]} as there is no custom due date."
+    puts "Skipping for #{issue["key"]} as there is no field of #{config['old_custom_field']}."
   end
   puts "======================="
 end
